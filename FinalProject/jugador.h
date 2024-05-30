ae408d356f760bd2c1ea488d11955b1111a734cd
Marcos Murillo Hernández <marcos.murillo@udea.edu.co>
2024-05-29 20:01:14 -0500

    Se agrega el personaje a la escena

--- FinalProject/jugador.h
+++ FinalProject/jugador.h
@@ -1,11 +1,22 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
+#include <QMainWindow>
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include <QPixmap>
+#include <QGraphicsScene>
 
-
-class jugador
+class jugador : public QObject, public QGraphicsPixmapItem
 {
+    Q_OBJECT
 public:
-    jugador();
-};
+    explicit jugador(QGraphicsScene* scene, QObject *parent = nullptr);
+    ~jugador();
 
-#endif // JUGADOR_H
+private:
+    void setup();
+
+signals:
+
+};
+#endif // jugador_H
--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QSplashScreen>
+#include <QIcon>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/imagenes/fondo_inicio.png"));
+    splash->show();
+
     MainWindow w;
-    w.show();
+    QTimer::singleShot(5000,splash,&QSplashScreen::close);
+    QTimer::singleShot(5100,&w,&MainWindow::show);
     return a.exec();
+
 }
